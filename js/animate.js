e91f5dff8c06b68551755c077f8c1d117f3d2ca0
sidjayisaok <sidjay18@gmail.com>
2016-12-05 22:05:06 -0500

    made animation files DRY, removed ES6 for Safari 9 and IE 11 compatibility

---
+++ js/animate.js
@@ -0,0 +1,54 @@
+//variables used for thumbnails
+var adiosClass = "animated lightSpeedOut";
+var turnClass = "animated flip";
+var site1 = '.site1';
+var site2 = '.site2';
+var site3 = '.site3';
+var site4 = '.site4';
+
+//variables used for social media buttons
+var flipClass = "animated zoomOut";
+var tadaClass = 'animated pulse';
+var image1 = '.image1';
+var image2 = '.image2';
+var image3 = '.image3';
+var image4 = '.image4';
+
+
+//DRY version to control image animation
+function logicAnim(param, classA, classB){
+
+    var endClass ='webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+     $(param).on({
+        'click': function(){
+            $(param).addClass(classA).one(endClass, function(){
+                $(this).removeClass(classA);
+            })
+        },
+
+        'mouseover': function(){
+            $(param).addClass(classB).one(endClass, function(){
+                $(this).removeClass(classB);
+            })
+        }
+    });
+}
+//call functions here for images
+logicAnim(site1, adiosClass, turnClass);
+
+logicAnim(site2, adiosClass, turnClass);
+
+logicAnim(site3, adiosClass, turnClass);
+
+logicAnim(site4, adiosClass, turnClass);
+
+//call functions here for social media
+logicAnim(image1, flipClass, tadaClass);
+
+logicAnim(image2, flipClass, tadaClass);
+
+logicAnim(image3, flipClass, tadaClass);
+
+logicAnim(image4, flipClass, tadaClass);
+
---
+++ js/triggerAnimation.js
@@ -0,0 +1,44 @@
+//using waypoint.js to trigger events
+$(document).ready(function(){
+
+ //variables used for transitions
+ var banner = '#banner';
+ var melange = '#melange';
+ var movieGazer = '#movieGazer';
+ var sniphy = '#sniphy';
+ var linkedin = '#linkedin';
+ var twitter = '#twitter';
+ var github = '#github';
+ var wordpress = '#wordpress';
+ var offsetA = '50%';
+ var offsetB = '90%';
+
+ //function to execute animations when they appear on the screen via waypoint.js
+ function callAnim(param, thisOffset){
+  // hide element on page load
+  $(param).css('opacity', 0);
+  //load element on scroll
+  $(param).waypoint(function() {
+      $(param).addClass('animated fadeInLeft');
+  }, { offset: thisOffset });
+ }
+
+//call the functions
+ callAnim(banner, offsetA);
+
+ callAnim(melange, offsetA);
+
+ callAnim(movieGazer, offsetA);
+
+ callAnim(sniphy, offsetA);
+
+ callAnim(linkedin, offsetB);
+
+ callAnim(twitter, offsetB);
+
+ callAnim(github, offsetB);
+
+ callAnim(wordpress, offsetB);
+
+
+});
